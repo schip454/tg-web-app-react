87f61ff166154a7a47446fd3322577f331b6b1ba
schip454 <118295365+schip454@users.noreply.github.com>
2023-12-12 04:24:59 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { useEffect } from "react";
 import "./App.css";
+import { useTelegram } from "./hooks/useTelegram";
 
 function App() {
+  const { tg, onToggleButton } = useTelegram();
+
   useEffect(() => {
     tg.ready();
   }, []);
 
   return (
     <div className="App">
-      work
-      <button onClick={onClose}>Зaкрыть</button>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
