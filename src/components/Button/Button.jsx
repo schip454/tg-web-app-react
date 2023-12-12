febed103e9fb681a011180221b8642ff6af9afa4
schip454 <118295365+schip454@users.noreply.github.com>
2023-12-12 19:47:09 +0500

    seven

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ import "./Button.css";
 const Button = (props) => {
   return (
     <button {...props} className={"button " + props.className}>
-      Button
+      {props.children}
     </button>
   );
 };
